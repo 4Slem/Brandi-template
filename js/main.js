9208b4c869026e7c920b127f262b39e1196f1d3d
Your Name <you@example.com>
2016-12-05 00:40:09 +0200

    paralax

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 (function () {
+	var textBlock = document.getElementsByClassName('item-text')[0];
+	var slider = document.getElementsByClassName('header-slider')[0];
+	var sliderHeight = slider.offsetHeight;
 	var elem = document.getElementById('header');
+
+
 	window.onscroll = function() {
+		//Навігація
   		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  		if(scrolled > 500){
+  		if(scrolled > (sliderHeight-50)){
   			elem.className = "header scroll-header";
+
   		}
-  		else {
+  		else{
   			elem.className = "header";
   		}
-  		var e = document.getElementsByClassName('item-text')[0];
-  		e.setAttribute("style", "transform: translate(0%, " + -scrolled/2 +"px " + ")");
+
+  		//Паралакс
+  		textBlock.setAttribute("style", "transform: translate(0%, " + -scrolled/2 +"px " + ")");
+  		slider.style.backgroundPosition = "0% " + scrolled*2 + "%";
+
 	}
 })();
