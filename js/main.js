63fe2547a732c1a2a1d41f7594a6517977458270
Your Name <you@example.com>
2016-12-05 00:57:06 +0200

    fdfd

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@
 	var sliderHeight = slider.offsetHeight;
 	var elem = document.getElementById('header');
 
-
+	if(window.onscroll) {
+		console.log("scroll")
+	}
 	window.onscroll = function() {
 		//Навігація
   		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
@@ -18,7 +20,7 @@
 
   		//Паралакс
   		textBlock.setAttribute("style", "transform: translate(0%, " + -scrolled/2 +"px " + ")");
-  		slider.style.backgroundPosition = "0% " + scrolled*2 + "%";
+  		slider.style.backgroundPosition = "0% " + -scrolled/2 + "px";
 
 	}
 })();
